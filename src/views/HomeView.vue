94297a7c03f18ddca15a21451f11f95d1acd13e6
phuoctt <phuoctt@rabiloo.com>
2024-11-30 11:53:06 +0700

    abcd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,83 +5,25 @@
         <img src="https://cdn6.agoda.net/images/MVC/default/background_image/illustrations/bg-agoda-homepage.png" alt="bg-homepage"/>
         <section class="hero">
           <h1>RONG CHƠI BỐN PHƯƠNG, GIÁ VẪN "YÊU THƯƠNG"</h1>
-          <nav class="nav1">
+          <div class="nav1">
             <ul>
               <li><a href="#Flight">🛫 Máy bay</a></li>
               <li><a href="#Hotel">🏨 Khách sạn</a></li>
               <li><a href="#Activity">🎢 Hoạt động</a></li>
               <li><a href="#Combo">🛫🏨🎢 Máy bay + K.sạn + H.động</a></li>
             </ul>
-          </nav>
-          <nav class="nav2">
-            <input v-model="searchQuery" placeholder="Where are you going?" />
-          <button @click="search">Search</button>
-          </nav>
+          </div>
+          <div class="nav2">
+            <div class="line1">
+              <div>Chỗ Ở Qua Đêm</div>
+              <div>Chỗ Ở Trong Ngày</div>
+            </div>
+            <span>🔍</span><input v-model="searchQuery" placeholder="Nhập điểm du lịch hoặc tên khách sạn" />
+          <button @click="search" class="search">Tìm</button>
+          </div>
         </section>
         <section class="popular-destinations">
           <h2>Popular Destinations</h2>
-          <ul>
-            <li v-for="(destination, index) in filteredDestinations" :key="index">
-              {{ destination.name }}
-            </li>
-          </ul>
-        </section>
-        <section class="popular-destinations">
-          <h2>Popular Destinations</h2>
-          <ul>
-            <li v-for="(destination, index) in filteredDestinations" :key="index">
-              {{ destination.name }}
-            </li>
-          </ul>
-        </section>
-        <section class="popular-destinations">
-          <h2>Popular Destinations</h2>
-          <ul>
-            <li v-for="(destination, index) in filteredDestinations" :key="index">
-              {{ destination.name }}
-            </li>
-          </ul>
-        </section>
-        <section class="popular-destinations">
-          <h2>Popular Destinations</h2>
-          <ul>
-            <li v-for="(destination, index) in filteredDestinations" :key="index">
-              {{ destination.name }}
-            </li>
-          </ul>
-        </section>
-        <section class="popular-destinations">
-          <h2>Popular Destinations</h2>
-          <ul>
-            <li v-for="(destination, index) in filteredDestinations" :key="index">
-              {{ destination.name }}
-            </li>
-          </ul>
-        </section>
-        <section class="popular-destinations">
-          <h2>Popular Destinations</h2>
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-
-          <h2>Popular Destinations</h2>
-
-
           <ul>
             <li v-for="(destination, index) in filteredDestinations" :key="index">
               {{ destination.name }}
@@ -135,27 +77,69 @@
     padding: 0;
     margin: 0;
   }
-  nav {
+  .nav1, .nav2 {
     margin-top: 15px;
-    display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: #fff;
     border-radius: 5px;
     padding: 10px 20px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
   }
   .nav1{
+    display: flex;
     position: relative;
     z-index: 1;
+    background-color: #fff;
   }
   .nav2 {
     position: absolute;
+    background-color: #f5f5f5;
     top: 70px;
     width: 100vh;
     left: -15vh;
-    height: 200px;
+    padding: 20px 40px;
     border-radius: 10px;
+  }
+  .line1 {
+    display: flex;
+    gap: 10px;
+  }
+  .line1 div {
+    cursor: pointer;
+    border-radius: 20px;
+    padding: 5px 10px;
+    background-color: #fff;
+    border: solid 1px #a1a1a1;
+  }
+  .nav2 span{
+    position: absolute;
+    font-size: 30px;
+    left: 50px;
+    top: 70px;
+    cursor: pointer;
+  }
+  .nav2 input {
+    margin-top: 10px;
+    margin-bottom: 20px;
+    width: 100%;
+    padding: 20px;
+    padding-left: 60px;
+    border-radius: 5px;
+    font-size: 15px;
+    border: solid 1px #a1a1a1;
+  }
+  .search {
+    position: absolute;
+    width: 40%;
+    height: 50px;
+    border-radius: 10px;
+    border: none;
+    background-color: #4787ff;
+    color: #fff;
+    font-size: 20px;
+    left: 30%;
+    top: 85%;
+    cursor: pointer;
   }
   ul {
     list-style: none;
